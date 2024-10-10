9be8d9512949289e32a6d9545a5e13079b9ec3c5
Carlos Guzmán <guzmanc@utb.edu.co>
2024-10-10 16:32:41 -0500

    nodejs despleigue automático

---
+++ README.md
@@ -0,0 +1,24 @@
+# Segundo Parcial – Infraestructura para TI
+
+## Objetivo
+
+Desarrollar una solución para automatizar el aprovisionamiento y configuración de la infraestructura necesaria para el despliegue de una API Web en Node.js que interactúa con una base de datos MySQL, utilizando Azure App Service.
+
+## Descripción del Proyecto
+
+El proyecto consiste en desplegar una API Web en Node.js que utiliza Express y MySQL. La aplicación se desplegará en un App Service de Azure, el cual requiere la configuración de un App Service Plan que soporte la infraestructura de la aplicación.
+
+**Recursos:**
+- [Build Node.js Rest APIs with Express & MySQL - BezKoder](https://www.bezkoder.com/node-js-express-mysql-crud/)
+- [Código completo en GitHub: bezkoder/nodejs-express-mysql](https://github.com/bezkoder/nodejs-express-mysql)
+
+## Pasos Realizados
+
+### 1. Preparación del Entorno
+
+![Captura 1](./screens/1.jpeg)
+
+**Comando:**
+```bash
+# Instalación de Azure CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "digitalocean_droplet" "mysql" {
   name     = "mysql-droplet"
   region   = "nyc1" # Elige una región cercana a ti
   size     = "s-1vcpu-1gb" # Ajusta el tamaño del droplet
-  ssh_keys = ["81:82:8a:89:28:50:ac:ed:01:7f:d7:52:c9:11:b6:90"]
+  ssh_keys = ["7b:4f:8b:d7:ce:11:10:88:b1:e3:45:11:6d:a4:f2:5c"]
 }
 
 
@@ -34,7 +34,7 @@ resource "digitalocean_droplet" "nodejs" {
   name     = "nodejs-droplet"
   region   = "nyc1"
   size     = "s-1vcpu-1gb"
-  ssh_keys = ["81:82:8a:89:28:50:ac:ed:01:7f:d7:52:c9:11:b6:90"]
+  ssh_keys = ["7b:4f:8b:d7:ce:11:10:88:b1:e3:45:11:6d:a4:f2:5c"]
 }
 
 
@@ -42,113 +42,84 @@ resource "digitalocean_droplet" "nodejs" {
 
 
 
-#Aislar la conexión a la BD de tal manera que solo se tenga acceso desde la aplicación web (App Service). Esto se logra configurando del firewall de la BD MySQL)
+# Aislar la conexión a la BD de tal manera que solo se tenga acceso desde la aplicación web (App Service). Esto se logra configurando del firewall de la BD MySQL)
+
+# Firewall para mysql
 
 resource "digitalocean_firewall" "mysql" {
   name = "mysql-firewall"
 
   droplet_ids = [digitalocean_droplet.mysql.id]
 
+  # Permitir conexión MySQL solo desde el servidor de Node.js
   inbound_rule {
     protocol         = "tcp"
     port_range       = "3306"
-    source_addresses = ["digitalocean_droplet.nodejs.ipv4_address"]
+    source_addresses = [digitalocean_droplet.nodejs.ipv4_address]
   }
+
+  # Permitir SSH solo desde la máquina de control
   inbound_rule {
     protocol         = "tcp"
     port_range       = "22"
-    source_addresses = ["191.95.132.34"]
+    source_addresses = ["191.95.132.34"] #mi ip 
   }
 
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "80"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "icmp"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
+  # Reglas de salida estándar (Internet)
   outbound_rule {
     protocol              = "tcp"
-    port_range            = "53"
+    port_range            = "443" # HTTPS
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   outbound_rule {
     protocol              = "udp"
-    port_range            = "53"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol              = "icmp"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-
-  }
-  # outbound_rule {
-  # protocol              = "tcp"
-  # port_range            = "0-65535"
-  # destination_addresses = ["0.0.0.0/0"]
-  # }
-  }
-
-
-
-
-
-resource "digitalocean_firewall" "nodejs" {
-  name = "nodejs-firewall"
-
-  droplet_ids = [digitalocean_droplet.mysql.id]
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "22"
-    source_addresses = ["192.168.1.0/24", "2002:1:2::/48"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "80"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "icmp"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol              = "tcp"
-    port_range            = "53"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol              = "udp"
-    port_range            = "53"
-    destination_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  outbound_rule {
-    protocol              = "icmp"
+    port_range            = "53" # DNS (53) para consultas
     destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 }
 
 
+
+# Firewall para Node.js
+
+resource "digitalocean_firewall" "nodejs" {
+  name = "nodejs-firewall"
+
+  # Reglas de entrada para HTTPS (puerto 443)
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443" # Solo HTTPS
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # Reglas de salida para HTTPS y DNS
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "443" # HTTPS
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53" # DNS (53) para consultas
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+
+
+# error generado cuando dejo la salida de internet (era porque en el port_ range no lo tenía definido y estaban varios en uno solo)
+# Error: Error creating firewall: POST https://api.digitalocean.com/v2/firewalls: 422 (request "88652950-0884-4617-9dc3-fbf318574783") invalid port range
+# │
+# │   with digitalocean_firewall.mysql,
+# │   on main.tf line 49, in resource "digitalocean_firewall" "mysql":
+# │   49: resource "digitalocean_firewall" "mysql" {
+# │
+# ╵
+# ╷
+# │ Error: Error creating firewall: POST https://api.digitalocean.com/v2/firewalls: 422 (request "be62e54b-d5d7-4151-9d13-066082b8bdce") invalid port range
+# │
+# │   with digitalocean_firewall.nodejs,
+# │   on main.tf line 86, in resource "digitalocean_firewall" "nodejs":
+# │   86: resource "digitalocean_firewall" "nodejs" {
